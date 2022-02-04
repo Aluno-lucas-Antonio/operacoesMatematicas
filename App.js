051cf8e0c2a167b8ce4f0bce42997b802b687400
Aluno-lucas-Antonio <lucasbielemann@gmail.com>
2022-02-04 16:10:17 -0300

    terceiro commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  * as React from 'react';
+import{Text, TouchableOpacity,TextInput, View, StyleSheet, StatusBar} from 'react-native'
+import estilo from './estilo';
+export default class App extends React.Component{
+  state={
+    valor1:0.0,
+    valor2:0.0,
+    resultado:0.0
+  }
+  atualizaValor1=(nunber)=>{
+    this.setState({valor1:nunber})
+  }
+  atualizaValor2=(nunber)=>{
+    this.setState({valor2:nunber})
+  }
+  soma(){
+    this.state.resultado=parseFloat(this.state.valor1)+
+    parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+  subtratacao(){
+    this.state.resultado=parseFloat(this.state.valor1)-
+    parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+  multiplicacao(){
+    this.state.resultado=parseFloat(this.state.valor1)*
+    parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+  divisao(){
+    this.state.resultado=parseFloat(this.state.valor1)/
+    parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  render(){
+    return(
+      <View style={estilo.container}>
+      <StatusBar backgroundColor="black"/>
+      <TextInput underlineColorAndroid="transparent"
+      placeholder="Digite o valor 1"
+      placeholderTextColor="black"
+      autoCapitalize="nome"
+      onChangeText={this.atualizaValor1}
+     keyboardType='numeric'/>
+     <StatusBar backgroundColor="black"/>
+   
+     <TextInput underlineColorAndroid="transparent"
+     placeholder="Digite o valor 2"
+     placeholderTextColor="black"
+     autoCapitalize="nome"
+     onChangeText={this.atualizaValor2}
+    keyboardType='numeric'/>
+
+      </View>
+    );
+    
+
+      }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ estilo.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native-web";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+export default StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor:'',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textoPreto:{
+        color:"black"
+    }
+});
